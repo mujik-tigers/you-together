c839d95d6f9689d331bca1842568ce041ec81010
Hyun <ghkdgus29@naver.com>
2024-03-07 16:44:28 +0900

    init: spring boot 프로젝트 생성

---
+++ src/main/java/site/youtogether/YouTogetherApplication.java
@@ -0,0 +1,13 @@
+package site.youtogether;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTogetherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouTogetherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/youtogether/YouTogetherApplicationTests.java
@@ -0,0 +1,13 @@
+package site.youtogether;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTogetherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
