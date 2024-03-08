9c8ba9178e21ca4b3d005db4eef59ec8a1a6ab9e
yeonise <cerealnum@gmail.com>
2024-03-08 21:15:47 +0900

    modify: [RoomService] repository 구조 변경에 따른 방 생성 단계 수정

--- src/main/java/site/youtogether/room/application/RoomService.java
+++ src/main/java/site/youtogether/room/application/RoomService.java
@@ -6,23 +6,30 @@ import lombok.RequiredArgsConstructor;
 import site.youtogether.room.Room;
 import site.youtogether.room.dto.RoomCode;
 import site.youtogether.room.dto.RoomSettings;
-import site.youtogether.room.storage.RoomStorage;
+import site.youtogether.room.infrastructure.RedisStorage;
+import site.youtogether.room.infrastructure.RoomStorage;
 import site.youtogether.user.Role;
 import site.youtogether.user.User;
-import site.youtogether.util.RandomUtils;
+import site.youtogether.user.infrastructure.UserStorage;
+import site.youtogether.util.RandomUtil;
 
 @Service
 @RequiredArgsConstructor
 public class RoomService {
 
+	private final RedisStorage redisStorage;
 	private final RoomStorage roomStorage;
+	private final UserStorage userStorage;
 
 	public RoomCode create(String address, RoomSettings roomSettings) {
 		Room room = createRoom(roomSettings);
 		User host = createHost(address);
-		room.addParticipant(address, host);
 
-		roomStorage.save(address, room);
+		roomStorage.save(room);
+		userStorage.save(host);
+
+		redisStorage.addHostingAddress(address);
+		redisStorage.addParticipant(room.getCode(), address);
 
 		return new RoomCode(room);
 	}
@@ -38,7 +45,7 @@ public class RoomService {
 	private User createHost(String address) {
 		return User.builder()
 			.address(address)
-			.nickname(RandomUtils.generateUserNickname())
+			.nickname(RandomUtil.generateUserNickname())
 			.role(Role.HOST)
 			.build();
 	}
