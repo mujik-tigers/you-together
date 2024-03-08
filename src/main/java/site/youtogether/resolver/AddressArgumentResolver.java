9910f82aa2ef268ef6582b1ee5376d1fe2c3bff3
yeonise <cerealnum@gmail.com>
2024-03-08 16:46:35 +0900

    feat: [Resolver] Address 애너테이션을 통해 IP를 전달하는 AddressResolver 생성

---
+++ src/main/java/site/youtogether/resolver/AddressArgumentResolver.java
@@ -0,0 +1,32 @@
+package site.youtogether.resolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Component
+public class AddressArgumentResolver implements HandlerMethodArgumentResolver {
+
+	@Override
+	public boolean supportsParameter(MethodParameter parameter) {
+		boolean hasAddressAnnotation = parameter.hasParameterAnnotation(Address.class);
+		boolean isStringType = String.class.isAssignableFrom(parameter.getParameterType());
+
+		return hasAddressAnnotation && isStringType;
+	}
+
+	@Override
+	public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest,
+		WebDataBinderFactory binderFactory) throws Exception {
+		HttpServletRequest request = webRequest.getNativeRequest(HttpServletRequest.class);
+
+		assert request != null;
+		return request.getLocalAddr();
+	}
+
+}
