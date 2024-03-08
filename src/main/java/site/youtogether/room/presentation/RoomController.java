17c064daa994dedf5f44fcbad3340f801d2e0afa
yeonise <cerealnum@gmail.com>
2024-03-08 16:59:11 +0900

    feat: [RoomController] 방 생성 요청에 대한 응답 수정

--- src/main/java/site/youtogether/room/presentation/RoomController.java
+++ src/main/java/site/youtogether/room/presentation/RoomController.java
@@ -5,14 +5,21 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import site.youtogether.resolver.Address;
+import site.youtogether.room.application.RoomService;
+import site.youtogether.room.dto.RoomCode;
 import site.youtogether.room.dto.RoomSettings;
 
 @RestController
+@RequiredArgsConstructor
 public class RoomController {
 
+	private final RoomService roomService;
+
 	@PostMapping("/rooms")
-	public String createRoom(@Valid @RequestBody RoomSettings roomSettings) {
-		return "test success";
+	public RoomCode createRoom(@Address String address, @Valid @RequestBody RoomSettings roomSettings) {
+		return roomService.create(address, roomSettings);
 	}
 
 }
