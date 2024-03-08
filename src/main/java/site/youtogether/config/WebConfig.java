d08ba10774a87910fd15765db8e5cd8ec4e49c6e
yeonise <cerealnum@gmail.com>
2024-03-08 16:50:05 +0900

    feat: [WebConfig] IP를 전달하는 Resolver 등록

--- src/main/java/site/youtogether/config/WebConfig.java
+++ src/main/java/site/youtogether/config/WebConfig.java
@@ -1,17 +1,22 @@
 package site.youtogether.config;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
 import site.youtogether.interceptor.HostingRestrictionInterceptor;
+import site.youtogether.resolver.AddressArgumentResolver;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
 	private final HostingRestrictionInterceptor hostingRestrictionInterceptor;
+	private final AddressArgumentResolver addressArgumentResolver;
 
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
@@ -19,4 +24,9 @@ public class WebConfig implements WebMvcConfigurer {
 			.addPathPatterns("/rooms");
 	}
 
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+		resolvers.add(addressArgumentResolver);
+	}
+	
 }
