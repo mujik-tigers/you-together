e2c9786c713ddc2e683c7f9068e0df96f7597efd
yeonise <cerealnum@gmail.com>
2024-03-10 14:41:49 +0900

    remove: 인터셉터 제거

--- src/main/java/site/youtogether/config/WebConfig.java
+++ src/main/java/site/youtogether/config/WebConfig.java
@@ -4,29 +4,20 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
-import site.youtogether.interceptor.HostingRestrictionInterceptor;
 import site.youtogether.resolver.AddressArgumentResolver;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-	private final HostingRestrictionInterceptor hostingRestrictionInterceptor;
 	private final AddressArgumentResolver addressArgumentResolver;
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(hostingRestrictionInterceptor)
-			.addPathPatterns("/rooms");
-	}
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(addressArgumentResolver);
 	}
-	
+
 }
