60b8012e4c2f05bd0b6f04347fdb3d590d6b5ae0
yeonise <cerealnum@gmail.com>
2024-03-14 16:24:47 +0900

    modify: [RedisConfig] Redis 설정 변경

--- src/main/java/site/youtogether/config/RedisConfig.java
+++ src/main/java/site/youtogether/config/RedisConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -18,7 +19,10 @@ public class RedisConfig {
 
 	@Bean
 	public RedisConnectionFactory redisConnectionFactory() {
-		return new LettuceConnectionFactory(redisProperties.getHost(), redisProperties.getPort());
+		RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
+		redisStandaloneConfiguration.setPassword(redisProperties.getPassword());
+		
+		return new LettuceConnectionFactory(redisStandaloneConfiguration);
 	}
 
 	@Bean
