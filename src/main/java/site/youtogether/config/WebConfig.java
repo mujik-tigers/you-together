d32d0ba40c1f09ed23fddb9d9c3f1bfcd604e348
yeonise <cerealnum@gmail.com>
2024-03-15 18:36:11 +0900

    feat: SessionCookieInterceptor 구현
    세션 쿠키를 확인하여 세션 코드를 전달합니다.
    세션 쿠키가 없다면 새로운 세션을 생성하고 응답에 포함시킵니다.

--- src/main/java/site/youtogether/config/WebConfig.java
+++ src/main/java/site/youtogether/config/WebConfig.java
@@ -4,20 +4,29 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
-import site.youtogether.resolver.AddressArgumentResolver;
+import site.youtogether.interceptor.SessionCookieInterceptor;
+import site.youtogether.resolver.SessionCodeArgumentResolver;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-	private final AddressArgumentResolver addressArgumentResolver;
+	private final SessionCookieInterceptor sessionCookieInterceptor;
+	private final SessionCodeArgumentResolver sessionCodeArgumentResolver;
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(sessionCookieInterceptor)
+			.addPathPatterns("/**");
+	}
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(addressArgumentResolver);
+		resolvers.add(sessionCodeArgumentResolver);
 	}
 
 }
