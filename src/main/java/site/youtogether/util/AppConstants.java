8d47e76b4d839c5e622b533e77caf6228597c9b1
yeonise <cerealnum@gmail.com>
2024-03-18 14:46:21 +0900

    rename: 상수 이름 변경
    SESSION_CODE → SESSION_COOKIE_NAME

--- src/main/java/site/youtogether/util/AppConstants.java
+++ src/main/java/site/youtogether/util/AppConstants.java
@@ -9,6 +9,6 @@ public final class AppConstants {
 	public static final String PARTICIPANTS_KEY = "participants";
 	public static final String ROOM_KEY_PREFIX = "room:";
 	public static final String USER_KEY_PREFIX = "user:";
-	public static final String SESSION_CODE = "SESSION_CODE";
+	public static final String SESSION_COOKIE_NAME = "YTSession";
 
 }
