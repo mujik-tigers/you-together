57eac7b0891317d062e5177ca1f1a9dfc5962df1
yeonise <cerealnum@gmail.com>
2024-03-18 14:42:49 +0900

    modify: WebConfig에 변경사항 반영

--- src/main/java/site/youtogether/config/WebConfig.java
+++ src/main/java/site/youtogether/config/WebConfig.java
@@ -4,29 +4,20 @@ import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
-import site.youtogether.interceptor.SessionCookieInterceptor;
-import site.youtogether.resolver.SessionCodeArgumentResolver;
+import site.youtogether.resolver.AddressArgumentResolver;
 
 @Configuration
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-	private final SessionCookieInterceptor sessionCookieInterceptor;
-	private final SessionCodeArgumentResolver sessionCodeArgumentResolver;
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(sessionCookieInterceptor)
-			.addPathPatterns("/**");
-	}
+	private final AddressArgumentResolver addressArgumentResolver;
 
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
-		resolvers.add(sessionCodeArgumentResolver);
+		resolvers.add(addressArgumentResolver);
 	}
 
 }
