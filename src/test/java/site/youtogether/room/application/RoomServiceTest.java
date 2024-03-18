933dd66076ca9da93cef4f878b27908d41443106
yeonise <cerealnum@gmail.com>
2024-03-18 21:58:14 +0900

    test: 서비스 테스트 구현 - 방 생성 성공

---
+++ src/test/java/site/youtogether/room/application/RoomServiceTest.java
@@ -0,0 +1,69 @@
+package site.youtogether.room.application;
+
+import static org.assertj.core.api.Assertions.*;
+import static site.youtogether.util.AppConstants.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import site.youtogether.IntegrationTestSupport;
+import site.youtogether.room.Room;
+import site.youtogether.room.dto.RoomCode;
+import site.youtogether.room.dto.RoomSettings;
+import site.youtogether.room.infrastructure.RoomStorage;
+import site.youtogether.user.Role;
+import site.youtogether.user.User;
+import site.youtogether.user.infrastructure.UserStorage;
+
+class RoomServiceTest extends IntegrationTestSupport {
+
+	@Autowired
+	private RoomService roomService;
+
+	@Autowired
+	private RoomStorage roomStorage;
+
+	@Autowired
+	private UserStorage userStorage;
+
+	@BeforeEach
+	void clean() {
+		roomStorage.deleteAll();
+		userStorage.deleteAll();
+	}
+
+	@Test
+	@DisplayName("새로운 방과 해당 방의 HOST를 생성할 수 있다")
+	void createSuccess() {
+		// given
+		String sessionCode = "7644a835e52e45dfa385";
+		String address = "127.0.0.1";
+		RoomSettings roomSettings = RoomSettings.builder()
+			.capacity(10)
+			.title("재밌는 쇼츠 같이 보기")
+			.password(null)
+			.build();
+
+		// when
+		RoomCode roomCode = roomService.create(sessionCode, address, roomSettings);
+
+		// then
+		Room room = roomStorage.findById(roomCode.getRoomCode()).orElseThrow();
+		User user = userStorage.findById(sessionCode).orElseThrow();
+
+		assertThat(roomCode.getRoomCode()).hasSize(ROOM_CODE_LENGTH);
+		assertThat(roomCode.getRoomCode()).isEqualTo(room.getCode());
+		assertThat(room.getCapacity()).isEqualTo(10);
+		assertThat(room.getTitle()).isEqualTo("재밌는 쇼츠 같이 보기");
+		assertThat(room.getPassword()).isNull();
+		assertThat(room.getHost().getSessionCode()).isEqualTo(sessionCode);
+		assertThat(room.getParticipants()).hasSize(1);
+		assertThat(user.getSessionCode()).isEqualTo(sessionCode);
+		assertThat(user.getAddress()).isEqualTo(address);
+		assertThat(user.getNickname()).isNotBlank();
+		assertThat(user.getRole()).isEqualTo(Role.HOST);
+	}
+
+}
