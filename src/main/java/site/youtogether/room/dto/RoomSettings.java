a8b230dc2bf9d09c7d6379e4aea3fee1e7709f6e
yeonise <cerealnum@gmail.com>
2024-03-20 21:02:16 +0900

    feat: [RoomSettings] validation message 설정

--- src/main/java/site/youtogether/room/dto/RoomSettings.java
+++ src/main/java/site/youtogether/room/dto/RoomSettings.java
@@ -13,14 +13,14 @@ import lombok.NoArgsConstructor;
 @Getter
 public class RoomSettings {
 
-	@NotBlank
-	@Size(min = 1, max = 30)
+	@NotBlank(message = "공백이 아닌 문자를 1개 이상 입력해 주세요")
+	@Size(min = 1, max = 30, message = "제목은 {min}자 이상 {max}자 이하로 입력해 주세요")
 	private String title;
 
-	@Range(min = 2, max = 10)
+	@Range(min = 2, max = 10, message = "정원은 {min}명 이상 {max}명 이하로 입력해 주세요")
 	private int capacity;
 
-	@Pattern(regexp = "^[0-9a-zA-Z]{5,10}$")
+	@Pattern(regexp = "^[0-9a-zA-Z]{5,10}$", message = "비밀번호는 5자 이상 10자 이하의 영문 또는 숫자로 입력해 주세요")
 	private String password;
 
 	@Builder
