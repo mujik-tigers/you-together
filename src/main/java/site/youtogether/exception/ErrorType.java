87be5574105b9f9ec9a912b418d1c1ae435ff160
yeonise <cerealnum@gmail.com>
2024-03-24 21:17:25 +0900

    modify: ErrorMessage 수정

--- src/main/java/site/youtogether/exception/ErrorType.java
+++ src/main/java/site/youtogether/exception/ErrorType.java
@@ -8,8 +8,8 @@ import lombok.Getter;
 public enum ErrorType {
 
 	// Room
-	SINGLE_ROOM_PARTICIPATION_VIOLATION(HttpStatus.BAD_REQUEST, "하나의 방에만 참가할 수 있습니다"),
-	USER_NOT_IN_ROOM(HttpStatus.BAD_REQUEST, "방에 참가 중인 사용자가 아닙니다"),
+	SINGLE_ROOM_PARTICIPATION_VIOLATION(HttpStatus.BAD_REQUEST, "하나의 방에만 참여할 수 있습니다"),
+	USER_NOT_IN_ROOM(HttpStatus.BAD_REQUEST, "방에 참여 중인 사용자가 아닙니다"),
 	ROOM_NOT_FOUND(HttpStatus.BAD_REQUEST, "존재하지 않는 방입니다");
 
 	private final HttpStatus status;
