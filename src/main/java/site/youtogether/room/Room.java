851cccb333f6f635d0f04fc552888ce7c695b792
yeonise <cerealnum@gmail.com>
2024-03-24 21:16:22 +0900

    feat: [Room] 방 들어가기 인터페이스 구현

--- src/main/java/site/youtogether/room/Room.java
+++ src/main/java/site/youtogether/room/Room.java
@@ -40,8 +40,13 @@ public class Room {
 		participants.put(host.getSessionCode(), host);
 	}
 
+	public void enter(User user) {
+		// TODO: 정원을 초과하여 입장할 수 없다
+		participants.put(user.getSessionCode(), user);
+	}
+
 	public User leave(String sessionCode) {
-		// TODO: 1. 나가고자 하는 사용자가 HOST인 경우 / 2. 나가면 참가자가 0명이 되는 경우
+		// TODO: 1. 나가고자 하는 사용자가 HOST인 경우 / 2. 나가면 참여자가 0명이 되는 경우
 		return participants.remove(sessionCode);
 	}
 
