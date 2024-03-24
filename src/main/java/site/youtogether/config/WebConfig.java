66840cac5cf38904be610429910f5dddb86592c3
yeonise <cerealnum@gmail.com>
2024-03-24 16:14:39 +0900

    feat: [WebConfig] 로컬 환경 기준 CORS 설정 추가

--- src/main/java/site/youtogether/config/WebConfig.java
+++ src/main/java/site/youtogether/config/WebConfig.java
@@ -3,7 +3,9 @@ package site.youtogether.config;
 import java.util.List;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import lombok.RequiredArgsConstructor;
@@ -20,4 +22,14 @@ public class WebConfig implements WebMvcConfigurer {
 		resolvers.add(addressArgumentResolver);
 	}
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**")
+			.allowCredentials(true)
+			.allowedOrigins("http://localhost:8081")
+			.allowedMethods(
+				HttpMethod.HEAD.name(), HttpMethod.GET.name(), HttpMethod.POST.name(),
+				HttpMethod.PUT.name(), HttpMethod.PATCH.name(), HttpMethod.DELETE.name(),
+				HttpMethod.OPTIONS.name());
+	}
 }
