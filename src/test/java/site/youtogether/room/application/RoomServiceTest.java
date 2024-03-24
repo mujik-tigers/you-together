dc69f5593ea64756addb2f878ac4333132c2d939
yeonise <cerealnum@gmail.com>
2024-03-24 21:18:46 +0900

    test: 서비스 테스트 구현 - 방 나가기 성공 및 예외

--- src/test/java/site/youtogether/room/application/RoomServiceTest.java
+++ src/test/java/site/youtogether/room/application/RoomServiceTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import site.youtogether.IntegrationTestSupport;
+import site.youtogether.exception.ErrorType;
+import site.youtogether.exception.room.UserNotInRoomException;
 import site.youtogether.room.Room;
 import site.youtogether.room.dto.RoomCode;
 import site.youtogether.room.dto.RoomSettings;
@@ -64,6 +66,62 @@ class RoomServiceTest extends IntegrationTestSupport {
 		assertThat(user.getAddress()).isEqualTo(address);
 		assertThat(user.getNickname()).isNotBlank();
 		assertThat(user.getRole()).isEqualTo(Role.HOST);
+	}
+
+	@Test
+	@DisplayName("방에서 나가면 세션이 종료된다")
+	void leaveSuccess() {
+		// given
+		Room room = createRoom();    // Creating a room with a host
+		User guest = User.builder()    // Creating a guest and entering the room
+			.sessionCode("1644a835e52e45dfa381")
+			.role(Role.GUEST)
+			.build();
+		room.enter(guest);
+
+		roomStorage.save(room);    // Saving to the database
+		userStorage.save(guest);
+
+		// when
+		roomService.leave(room.getCode(), guest.getSessionCode());    // The entered guest leaves the room
+
+		// then
+		Room leftRoom = roomStorage.findById(room.getCode()).get();
+
+		assertThat(leftRoom.getParticipants()).hasSize(1);
+		assertThat(leftRoom.getParticipants()).doesNotContainKey(guest.getSessionCode());
+		assertThat(userStorage.existsById(guest.getSessionCode())).isFalse();
+	}
+
+	@Test
+	@DisplayName("방 참여자가 아닌 사용자가 나가려고 할 때 예외가 발생한다")
+	void leaveFail_UserNotInRoom() {
+		// given
+		Room room = createRoom();
+		roomStorage.save(room);
+
+		// when / then
+		assertThatThrownBy(() -> roomService.leave(room.getCode(), "1644a835e52e45dfa381"))
+			.isInstanceOf(UserNotInRoomException.class)
+			.hasMessage(ErrorType.USER_NOT_IN_ROOM.getMessage());
+	}
+
+	private Room createRoom() {
+		User host = User.builder()
+			.sessionCode("7644a835e52e45dfa385")
+			.role(Role.HOST)
+			.build();
+
+		Room room = Room.builder()
+			.capacity(10)
+			.title("재밌는 쇼츠 같이 보기")
+			.password(null)
+			.host(host)
+			.build();
+
+		room.enter(host);
+
+		return room;
 	}
 
 }
