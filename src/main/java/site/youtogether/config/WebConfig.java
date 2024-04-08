9c402ec906662aca3bcb723d08b34fc4be4924c4
Seoyeon Kim <cerealnum@gmail.com>
2024-04-08 13:12:43 +0900

    feat: [WebConfig] Add allowed origins

--- src/main/java/site/youtogether/config/WebConfig.java
+++ src/main/java/site/youtogether/config/WebConfig.java
@@ -13,7 +13,7 @@ public class WebConfig implements WebMvcConfigurer {
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
 		registry.addMapping("/**")
-			.allowedOrigins("http://localhost:3000")
+			.allowedOrigins("http://localhost:3000", "https://you-together-web.vercel.app")
 			.allowedMethods("*")
 			.allowCredentials(true)
 			.maxAge(3000);
