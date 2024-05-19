3e7e5f7732da47add2cd707463cda014d2a15110
yeonise <cerealnum@gmail.com>
2024-05-19 16:18:13 +0900

    feat: 입장 시 현재 재생중인 영상 아이디 반환 추가

--- src/test/java/site/youtogether/room/presentation/RoomControllerTest.java
+++ src/test/java/site/youtogether/room/presentation/RoomControllerTest.java
@@ -341,7 +341,8 @@ class RoomControllerTest extends RestDocsSupport {
 		int capacity = 10;
 
 		Participant participantInfo = new Participant(10L, "황똥땡", Role.HOST);
-		RoomDetail createdRoomDetail = new RoomDetail(roomCode, roomTitle, participantInfo, capacity, 2, false, "aespa 에스파 'Supernova' MV", "SMTOWN");
+		RoomDetail createdRoomDetail = new RoomDetail(roomCode, roomTitle, participantInfo, capacity, 2, false, "phuiiNCxRMg", "aespa 에스파 'Supernova' MV",
+			"SMTOWN");
 		Optional<User> user = Optional.of(User.builder()
 			.currentRoomCode(null)
 			.build());
@@ -386,6 +387,7 @@ class RoomControllerTest extends RestDocsSupport {
 					fieldWithPath("data.capacity").type(JsonFieldType.NUMBER).description("정원"),
 					fieldWithPath("data.currentParticipant").type(JsonFieldType.NUMBER).description("현재 참가자 수"),
 					fieldWithPath("data.passwordExist").type(JsonFieldType.BOOLEAN).description("비밀번호 존재 여부"),
+					fieldWithPath("data.currentVideoId").type(JsonFieldType.STRING).description("현재 재생중인 영상의 아이디").optional(),
 					fieldWithPath("data.currentVideoTitle").type(JsonFieldType.STRING).description("현재 재생중인 영상의 제목").optional(),
 					fieldWithPath("data.currentChannelTitle").type(JsonFieldType.STRING).description("현재 재생중인 영상의 채널 이름").optional()
 				)
@@ -402,7 +404,8 @@ class RoomControllerTest extends RestDocsSupport {
 		int capacity = 10;
 
 		Participant participantInfo = new Participant(10L, "황똥땡", Role.HOST);
-		RoomDetail createdRoomDetail = new RoomDetail(roomCode, roomTitle, participantInfo, capacity, 2, false, "aespa 에스파 'Supernova' MV", "SMTOWN");
+		RoomDetail createdRoomDetail = new RoomDetail(roomCode, roomTitle, participantInfo, capacity, 2, false, "phuiiNCxRMg", "aespa 에스파 'Supernova' MV",
+			"SMTOWN");
 		Optional<User> user = Optional.of(User.builder()
 			.currentRoomCode("1e7050f7d7")
 			.build());
@@ -449,7 +452,8 @@ class RoomControllerTest extends RestDocsSupport {
 		int capacity = 10;
 
 		Participant participantInfo = new Participant(10L, "황똥땡", Role.HOST);
-		RoomDetail createdRoomDetail = new RoomDetail(roomCode, roomTitle, participantInfo, capacity, 2, true, "aespa 에스파 'Supernova' MV", "SMTOWN");
+		RoomDetail createdRoomDetail = new RoomDetail(roomCode, roomTitle, participantInfo, capacity, 2, true, "phuiiNCxRMg", "aespa 에스파 'Supernova' MV",
+			"SMTOWN");
 		Optional<User> user = Optional.of(User.builder()
 			.currentRoomCode(null)
 			.build());
@@ -499,6 +503,7 @@ class RoomControllerTest extends RestDocsSupport {
 					fieldWithPath("data.capacity").type(JsonFieldType.NUMBER).description("정원"),
 					fieldWithPath("data.currentParticipant").type(JsonFieldType.NUMBER).description("현재 참가자 수"),
 					fieldWithPath("data.passwordExist").type(JsonFieldType.BOOLEAN).description("비밀번호 존재 여부"),
+					fieldWithPath("data.currentVideoId").type(JsonFieldType.STRING).description("현재 재생중인 영상의 아이디").optional(),
 					fieldWithPath("data.currentVideoTitle").type(JsonFieldType.STRING).description("현재 재생중인 영상의 제목").optional(),
 					fieldWithPath("data.currentChannelTitle").type(JsonFieldType.STRING).description("현재 재생중인 영상의 채널 이름").optional()
 				)
