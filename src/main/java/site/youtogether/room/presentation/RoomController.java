fdf0ee258ec78e7b9d788b36c3203e85ba07743b
Hyun <ghkdgus29@naver.com>
2024-06-27 17:00:49 +0900

    log: 방 검색 시 파라미터로 넘어온 키워드 로깅

--- src/main/java/site/youtogether/room/presentation/RoomController.java
+++ src/main/java/site/youtogether/room/presentation/RoomController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import site.youtogether.room.application.RoomService;
 import site.youtogether.room.dto.ChangedRoomTitle;
 import site.youtogether.room.dto.NewRoom;
@@ -30,12 +31,14 @@ import site.youtogether.util.resolver.UserTracking;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class RoomController {
 
 	private final RoomService roomService;
 
 	@GetMapping("/rooms")
 	public ResponseEntity<ApiResponse<RoomList>> fetchRoomList(@PageableDefault Pageable pageable, @RequestParam(required = false) String keyword) {
+		log.info("넘어온 keyword:{}", keyword);
 		RoomList roomList = roomService.fetchAll(pageable, keyword);
 
 		return ResponseEntity.ok()
